961cb128b23b737da9f37790a5d2d33db78b8209
subbi <92204014+subinsad@users.noreply.github.com>
2023-12-29 15:05:44 +0900

    li 2개씩 찍히는 이슈발생

--- src/components/TodoCrudApi.js
+++ src/components/TodoCrudApi.js
@@ -19,6 +19,11 @@ export default class TodoCrudApi {
 
             const todos = await response.json();
 
+            if (todos === null || todos === undefined) {
+                console.error('undefined');
+                return [];
+            }
+
             return todos
                 .map((todo) => ({
                     ...todo,
@@ -28,6 +33,37 @@ export default class TodoCrudApi {
                 .sort((a, b) => b.createdAt - a.createdAt);
         } catch (error) {
             console.error('할 일 목록을 불러오는 중 오류 발생:', error);
+            throw error; // 에러를 호출한 쪽으로 전파합니다.
+        }
+    }
+
+    static async deleteTodo(todoId) {
+        const myHeaders = new Headers();
+        myHeaders.append('Content-Type', 'application/json');
+        myHeaders.append('apikey', 'KDT7_GrZ1eYBo');
+        myHeaders.append('username', 'KDT7_ParkSuBin');
+
+        const requestOptions = {
+            method: 'DELETE',
+            headers: myHeaders,
+            redirect: 'follow',
+        };
+
+        try {
+            const response = await fetch(
+                `https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos/${todoId}`,
+                requestOptions
+            );
+
+            if (!response.ok) {
+                const errorText = await response.text(); // 에러 응답 내용을 가져옵니다.
+                throw new Error(
+                    `Failed to delete todo ${todoId}. Server response: ${response.status} - ${errorText}`
+                );
+            }
+            console.log(`todo with id ${todoId} deleted`);
+        } catch (error) {
+            console.log('error:', error);
             throw error; // 에러를 호출한 쪽으로 전파합니다.
         }
     }
--- src/components/Todomaincon.js
+++ src/components/Todomaincon.js
@@ -4,16 +4,16 @@ import TodoCrudApi from './TodoCrudApi';
 export default class TodoMaincon extends Component {
     async render() {
         this.el.innerHTML = /* html */ `
-        <div class="Todolist">
-            <h3> To Do List </h3> 
-            <div class="contents" id="todoMainContainer">
-                <div class="input-box">
-                    <input type="text" class="todo-input" placeholder="할 일 추가">
-                    <button class="btn btn-normal add-todo">추가</button>                
+            <div class="Todolist">
+                <h3> To Do List </h3> 
+                <div class="contents" id="todoMainContainer">
+                    <div class="input-box">
+                        <input type="text" class="todo-input" placeholder="할 일 추가">
+                        <button class="btn btn-normal add-todo">추가</button>                
+                    </div>
+                    <ul id="todoItems"></ul>
                 </div>
-                <ul id="todoItems"></ul>
             </div>
-        </div>
         `;
 
         document.addEventListener('DOMContentLoaded', async () => {
@@ -23,6 +23,7 @@ export default class TodoMaincon extends Component {
             const addTodoButton = todoMainContainer.querySelector('.add-todo');
             const inputEl = todoMainContainer.querySelector('.todo-input');
 
+            // 버튼 클릭 시 투두 입력
             addTodoButton.addEventListener('click', async () => {
                 const newTodoTitle = inputEl.value;
                 if (newTodoTitle.trim() !== '') {
@@ -31,6 +32,7 @@ export default class TodoMaincon extends Component {
                 }
             });
 
+            // 엔터키로 입력
             inputEl.addEventListener('keyup', async (event) => {
                 if (event.key === 'Enter') {
                     const newTodoTitle = inputEl.value;
@@ -49,13 +51,38 @@ export default class TodoMaincon extends Component {
         });
     }
 
+    // 투두 추가
     async addTodo(newTodoTitle) {
         try {
             const newTodo = await TodoCrudApi.createTodo(newTodoTitle);
-            this.renderTodoItem(newTodo);
+            this.renderTodoItem(newTodo, this.el.querySelector('#todoItems'));
         } catch (error) {
             console.error('할 일을 추가하는 중 오류 발생:', error);
         }
+    }
+
+    renderTodoItem(todo, container) {
+        if (!container) {
+            console.error('container is undefined');
+            return;
+        }
+
+        const todoItem = document.createElement('li');
+        const todoContent = document.createElement('span');
+        const deleteBtn = document.createElement('button');
+
+        todoContent.textContent = todo.title;
+        deleteBtn.textContent = '삭제';
+
+        deleteBtn.addEventListener('click', async () => {
+            await this.deleteTodo(todo.id);
+            await this.refreshTodos();
+        });
+
+        todoItem.appendChild(todoContent);
+        todoItem.appendChild(deleteBtn);
+
+        container.appendChild(todoItem); // 전달받은 container에 직접 추가
     }
 
     async refreshTodos() {
@@ -69,9 +96,23 @@ export default class TodoMaincon extends Component {
 
     displayTodos(todos) {
         const todoItems = this.el.querySelector('#todoItems');
+        const existingTodoItems = todoItems.querySelectorAll('li');
+
+        existingTodoItems.forEach((existingTodo) => {
+            const todoId = existingTodo.dataset.todoId;
+            const isExistInTodos = todos.some((todo) => todo.id === todoId);
+
+            if (!isExistInTodos) {
+                existingTodo.remove();
+            }
+        });
 
         todos.forEach((todo) => {
-            if (!this.isTodoInUI(todo, todoItems)) {
+            const existingTodo = Array.from(existingTodoItems).find(
+                (el) => el.dataset.todoId === todo.id
+            );
+
+            if (!existingTodo) {
                 this.renderTodoItem(todo, todoItems);
             }
         });
@@ -88,12 +129,12 @@ export default class TodoMaincon extends Component {
         return false;
     }
 
-    renderTodoItem(todo, container) {
-        const todoItem = document.createElement('li');
-        const todoContent = document.createElement('span');
-
-        todoContent.textContent = todo.title;
-        todoItem.appendChild(todoContent);
-        container.appendChild(todoItem); // 전달받은 container에 직접 추가
+    async deleteTodo(todoId) {
+        try {
+            await TodoCrudApi.deleteTodo(todoId);
+        } catch (error) {
+            console.error('할 일을 삭제하는 중 오류 발생:', error);
+            throw error;
+        }
     }
 }
