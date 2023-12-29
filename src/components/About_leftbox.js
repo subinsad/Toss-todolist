6d67e2a37c0a1b66cf30a992d5bc1e78b14b4592
subbi <92204014+subinsad@users.noreply.github.com>
2023-12-29 23:31:04 +0900

    protfolio 퍼블리싱

---
+++ src/components/About_leftbox.js
@@ -0,0 +1,53 @@
+import { Component } from '../core/heropy';
+
+export default class LeftBox extends Component {
+    constructor() {
+        super({
+            tagName: 'article',
+        });
+    }
+
+    render() {
+        this.el.innerHTML = /* html */ `
+        <div class="left">
+            <div class="left__btn">
+                <p> Please press the heart </p>
+                <a href="#"> <span class="material-symbols-outlined">heart_plus</span></a>
+            </div>
+
+            <div class="left__contents-box"> 
+                <div class="left__contents-box__title">
+                    <p> Photo Folio </p>   
+                    <span class="material-symbols-outlined">expand_circle_right</span>
+                </div>
+
+                <ul class="left__contents-box__list">
+                    <li>
+                        <img src="https://cdn.paris.spl.li/wp-content/uploads/2023/11/pc-logo.svg" class="profile" alt="profile" width="40" height="auto">
+                        <div> 
+                            <p> clone codeing </p>
+                            <a href="#"> parisbaguette </a>
+                        </div>
+                    </li>
+
+                    <li>
+                        <span class="material-symbols-outlined">movie</span>
+                        <div> 
+                            <p> Js project </p>
+                            <a href="#"> movie-app </a>
+                        </div>
+                    </li>
+
+                    <li>
+                    <img src="src/img/nespresso.avif" class="profile" alt="profile" width="40" height="auto">
+                        <div> 
+                            <p> senier project </p>
+                            <a href="#"> Deeply </a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        `;
+    }
+}
--- src/components/About_skil.js
+++ src/components/About_skil.js
@@ -1,12 +1,15 @@
 import { Component } from '../core/heropy';
+import LeftBox from './About_leftbox';
 
 export default class Skil extends Component {
     render() {
         this.el.classList.add('SkilContainer');
+
+        const leftbox = new LeftBox();
+        const leftboxEl = leftbox.el;
+
         this.el.innerHTML = /* html */ `
-        <div class="text-box">
-            Hello
-        </div>
+            ${leftboxEl.innerHTML}
         `;
     }
 }
