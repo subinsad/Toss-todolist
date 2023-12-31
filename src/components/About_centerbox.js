b3af95d03adcec29248c216774aef15816701b2d
subbi <92204014+subinsad@users.noreply.github.com>
2023-12-31 17:58:56 +0900

    chart 구현

--- src/components/About_centerbox.js
+++ src/components/About_centerbox.js
@@ -1,4 +1,5 @@
 import { Component } from '../core/heropy';
+import Chart from 'chart.js/auto';
 
 export default class CenterBox extends Component {
     constructor() {
@@ -10,16 +11,52 @@ export default class CenterBox extends Component {
     render() {
         this.el.innerHTML = /* html */ `
         <div class="center">
-            <div> 
+            <div class="title"> 
                 <h3> My Skilset </h3>
                 <span class="material-symbols-outlined">settings</span>
             </div>
-            <p> improved it by <span>12 point </span> last week </p>
+            <p class="subTitle"> Improved it by <span>12 points</span> last week </p>
 
-            <div> 
-
+            <div class="chart"> 
+                <canvas id="pie-chart" width="230" height="230"></canvas>
             </div>
         </div>
         `;
+
+        document.addEventListener('DOMContentLoaded', () => {
+            new Chart(document.getElementById('pie-chart'), {
+                type: 'pie',
+                data: {
+                    labels: [
+                        'HTML',
+                        'CSS',
+                        'SCSS',
+                        'Javascript',
+                        'VUE.JS',
+                        'REACT',
+                    ],
+                    datasets: [
+                        {
+                            label: 'Used',
+                            backgroundColor: [
+                                '#E34F26',
+                                '#1572B6',
+                                '#CC6699',
+                                '#F7DF1E',
+                                '#4FC08D',
+                                '#61DAFB',
+                            ],
+                            data: [700, 600, 500, 400, 300, 100],
+                        },
+                    ],
+                },
+                options: {
+                    title: {
+                        display: true,
+                        text: 'Most Used Languages',
+                    },
+                },
+            });
+        });
     }
 }
