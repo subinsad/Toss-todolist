b9eb5bef39506099221952982b690522e1053045
subbi <92204014+subinsad@users.noreply.github.com>
2024-01-07 23:44:56 +0900

    render

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import TheHeader from './components/TheHeader';
 import MyPage from './components/MyPage';
 
 export default class App extends Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
     render() {
         const theHeader = new TheHeader().el;
         const routerView = document.createElement('router-view');
