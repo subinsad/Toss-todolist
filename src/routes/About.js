a8a31208cd5700c1d85220fa897fcb56dd9063a0
subbi <92204014+subinsad@users.noreply.github.com>
2024-01-08 22:49:06 +0900

    수정

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,13 +1,13 @@
 import { Component } from '../core/heropy';
 import Header from '../components/About_myprofile';
-import Skil from '../components/About_Skil';
+import About_Skil from '../components/About_skil.js';
 
 export default class About extends Component {
     render() {
         const headerComponent = new Header();
         const headerElement = headerComponent.el;
 
-        const skilComponent = new Skil();
+        const skilComponent = new About_Skil();
         const skilElement = skilComponent.el;
 
         this.el.appendChild(headerElement);
